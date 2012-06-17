1da83a0a16b74bbac07ffb60f5e3e3d11c06b81b
swoorup <swoorup@562435f6-89a1-033f-70e2-f487d5484f5a>
2012-06-17 07:40:44 +0000

    Added class defination for CMatrix
    git-svn-id: http://project-liberty.googlecode.com/svn/trunk@22 562435f6-89a1-033f-70e2-f487d5484f5a

---
+++ SOLCore/GameVC/CMatrix.h
@@ -0,0 +1,15 @@
+#ifndef __CMATRIX_H
+#define __CMATRIX_H
+
+#include "../StdInc.h
+#pragma pack(push, 1)
+
+class CMatrix {
+public:
+  RwMatrix rwMatrix;
+  void *matrixPtr;
+  int haveRwMatrix;
+};
+
+#pragma pack(pop)
+#endif

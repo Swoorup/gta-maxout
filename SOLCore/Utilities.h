2199bf5081cdfc2d67dfb023e285492537b8c548
swoorup <swoorup@562435f6-89a1-033f-70e2-f487d5484f5a>
2012-06-17 07:42:56 +0000

    added pad macros for use in Vice city native structure and class definations
    git-svn-id: http://project-liberty.googlecode.com/svn/trunk@26 562435f6-89a1-033f-70e2-f487d5484f5a

--- SOLCore/Utilities.h
+++ SOLCore/Utilities.h
@@ -3,6 +3,7 @@
 
 #define M_PI       3.14159265358979323846
 
+#define _pad(x,n) unsigned char x[n]
 namespace utl{
     template <typename T> inline const T abs(T const & x){
         return ( x<0 ) ? -x : x;

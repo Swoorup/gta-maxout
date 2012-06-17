82e3f64c4f82a8a4ea8d4edb513c17b45641a545
swoorup <swoorup@562435f6-89a1-033f-70e2-f487d5484f5a>
2012-06-17 07:45:45 +0000

    git-svn-id: http://project-liberty.googlecode.com/svn/trunk@28 562435f6-89a1-033f-70e2-f487d5484f5a

--- SOLCore/StdInc.h
+++ SOLCore/StdInc.h
@@ -2,25 +2,30 @@
 
 #include "main.h"
 
+// standard include directives for C++
 #include <Windows.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <math.h>
 
+// utilies directives for general usuage
 #include "CSingleton.h"
 #include "CMemory.h"
 #include "Utilities.h"
 #include "D3D8Hook.h"
 #include "CCustomFont.h"
 
+// to be disengaged
 #include "PathBoundariesHack.hpp"
-
 #include "GamePatches.h"
 
+// game definations and new patches includes
 #include "GameVC\CPathFindHook.h"
 #include "GameVC\CGameHookManager.h"
 #include "GameVC\RenderWare.h"
+#include "GameVC\CMatrix.h"
 #include "GameVC\CWorld.h"
+#include "GameVC\CCamera.h"
 #include "GameVC\GameVC.h"
 #include "GameVC\CFileMgr.h"
 #include "GameVC\CDebug.h"

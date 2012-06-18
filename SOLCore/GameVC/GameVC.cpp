b2560146491edd1f1d40dfe2b5228be58fa351ca
swoorup <swoorup@562435f6-89a1-033f-70e2-f487d5484f5a>
2012-06-18 16:04:47 +0000

    Added global instance of TheCamera. Expect to be moved to CGameVariables
    git-svn-id: http://project-liberty.googlecode.com/svn/trunk@31 562435f6-89a1-033f-70e2-f487d5484f5a

--- SOLCore/GameVC/GameVC.cpp
+++ SOLCore/GameVC/GameVC.cpp
@@ -1,15 +1,18 @@
 #include "GameVC.h"
 
-
 #define Func_FindPlayerCentreOfWorld_NoSniperShift 0x4BC020
 #define Func_FindPlayerPed                         0x4BC120
 #define Func_LoadingScreen                         0x4A69D0
 
 /*****************************************************************************/
-/** Game Common functions mapping						                    **/
+/** Game Global functions mapping						                    **/
 /*****************************************************************************/
 
 FindPlayerCentreOfWorld_NoSniperShift_t				    FindPlayerCentreOfWorld_NoSniperShift =				(FindPlayerCentreOfWorld_NoSniperShift_t)			    Func_FindPlayerCentreOfWorld_NoSniperShift;
 FindPlayerPed_t                                         FindPlayerPed                         =             (FindPlayerPed_t)                                       Func_FindPlayerPed;
-
 LoadingScreen_t                                         LoadingScreen                         =             (LoadingScreen_t)                                       Func_LoadingScreen;
+
+//----------------------------------------------------------------
+// Global Variables for the game
+//----------------------------------------------------------------
+CCamera& TheCamera = *(CCamera*)0x7E4688;
--- SOLCore/GameVC/GameVC.h
+++ SOLCore/GameVC/GameVC.h
@@ -1,13 +1,16 @@
 #ifndef GAMEVC_H
 #define GAMEVC_H
-#include "..\main.h"
+
+#include "..\StdInc.h"
 
 typedef RwV3d* (__cdecl *FindPlayerCentreOfWorld_NoSniperShift_t)(void);  
-extern FindPlayerCentreOfWorld_NoSniperShift_t FindPlayerCentreOfWorld_NoSniperShift;
-
+typedef void (__cdecl *LoadingScreen_t)(char const *szGameStateText, char const *szLoadingStageText, char const *szUnusedStr);
 typedef int (__cdecl *FindPlayerPed_t)(void);
-extern FindPlayerPed_t FindPlayerPed;
 
-typedef void (__cdecl *LoadingScreen_t)(char const *gameStateText, char const *loadingStageText, char const *);
+extern FindPlayerCentreOfWorld_NoSniperShift_t FindPlayerCentreOfWorld_NoSniperShift;
+extern FindPlayerPed_t FindPlayerPed;
 extern LoadingScreen_t LoadingScreen;
+
+extern CCamera& TheCamera;
+
 #endif

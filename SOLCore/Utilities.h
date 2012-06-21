fb4cb177f6ca3da1df5d4baf086614bb0af8e644
swoorup <swoorup@562435f6-89a1-033f-70e2-f487d5484f5a>
2012-06-21 10:01:59 +0000

    added OFFSETOF macro to get offset directly from structures and classes
    git-svn-id: http://project-liberty.googlecode.com/svn/trunk@47 562435f6-89a1-033f-70e2-f487d5484f5a

--- SOLCore/Utilities.h
+++ SOLCore/Utilities.h
@@ -4,6 +4,8 @@
 #define M_PI       3.14159265358979323846
 
 #define _pad(x,n) unsigned char x[n]
+#define OFFSETOF(type, field)    ((unsigned long) &(((type *) 0)->field))
+
 namespace utl{
     template <typename T> inline const T abs(T const & x){
         return ( x<0 ) ? -x : x;

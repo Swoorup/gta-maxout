0d06332f52e529688db9673bac7f27099324ec4c
swoorup <swoorup@562435f6-89a1-033f-70e2-f487d5484f5a>
2012-06-21 10:03:46 +0000

    small changes in the header
    git-svn-id: http://project-liberty.googlecode.com/svn/trunk@49 562435f6-89a1-033f-70e2-f487d5484f5a

--- SOLCore/GameVC/CPathFindHook.h
+++ SOLCore/GameVC/CPathFindHook.h
@@ -3,13 +3,15 @@ class CPathFindHook;
 #ifndef __CPATHFINDHOOK_H
 #define __CPATHFINDhOOK_H
 
+#include "../StdInc.h"
+
 class CPathFindHook : public CSingleton < CPathFindHook >
 {
 private:
     void RemoveHook(void);
 public:
-    CPathFindHook(void);
-    ~CPathFindHook(void);
+    CPathFindHook();
+    ~CPathFindHook();
 
     void ApplyHook(void);
 };

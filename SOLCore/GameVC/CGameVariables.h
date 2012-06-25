a56fd9706e1918cb1cead152215cac2a1058801e
swoorup <swoorup@562435f6-89a1-033f-70e2-f487d5484f5a>
2012-06-25 16:18:42 +0000

    added GetGamePathFindInstance to get the global instance of patched paths
    git-svn-id: http://project-liberty.googlecode.com/svn/trunk@67 562435f6-89a1-033f-70e2-f487d5484f5a

--- SOLCore/GameVC/CGameVariables.h
+++ SOLCore/GameVC/CGameVariables.h
@@ -1,15 +1,21 @@
 #ifndef __CGAMEVARIABLES_H
 #define __CGAMEVARIABLES_H
 
-#include "../StdInc.h"
+#include "../stdinc.h"
+#include "CPathFind.h"
+
+extern CPathFind* pThePaths;
 
 class CGameVariables {
 private:
     static float* VAR_ms_fFarClipZ;
     static float* VAR_ms_fNearClipZ;
+    static unsigned int* VAR_CTimer_m_snTimeInMilliseconds;
 public:
     static inline float GetFarClipZ() {return *VAR_ms_fFarClipZ;};
     static inline float GetNearClipZ() {return *VAR_ms_fNearClipZ;};
+    static inline unsigned int GetTimeInMilliseconds() {return *VAR_CTimer_m_snTimeInMilliseconds;};
+    static inline CPathFind* GetGamePathFindInstance() {return pThePaths;};
 };
 
 #endif

0ee3f629a492c00e888306d5cb53f849f8bb8f7a
swoorup <swoorup@562435f6-89a1-033f-70e2-f487d5484f5a>
2012-06-26 09:30:21 +0000

    removed minor unneeded type conversions
    git-svn-id: http://project-liberty.googlecode.com/svn/trunk@69 562435f6-89a1-033f-70e2-f487d5484f5a

--- SOLCore/GameVC/CGameHookManager.cpp
+++ SOLCore/GameVC/CGameHookManager.cpp
@@ -20,26 +20,26 @@ CGameHookManager::~CGameHookManager(void){
 
 void CGameHookManager::ApplyHook(void){
     // Hooked in CFileLoader::LoadLevel
-    CMemory::InstallCallHook( 0x48DD5D, (DWORD)&CFileLoader::LoadScene, ASM_CALL);
+    CMemory::InstallCallHook( 0x48DD5D, &CFileLoader::LoadScene, ASM_CALL);
     // Hooked in CGame::InitializeRenderware
-    CMemory::InstallCallHook( 0x4A5298, (DWORD)&CDebug::Initialize, ASM_CALL);
+    CMemory::InstallCallHook( 0x4A5298, &CDebug::Initialize, ASM_CALL);
     // Hooked in Idle(void*)
-    CMemory::InstallCallHook( 0x4A614C, (DWORD)&CDebug::DebugDisplayText, ASM_CALL);
+    CMemory::InstallCallHook( 0x4A614C, &CDebug::DebugDisplayText, ASM_CALL);
     // Hooked in CGame::ShutdownRenderware
-    CMemory::InstallCallHook( 0x4A5140, (DWORD)&CDebug::Shutdown, ASM_CALL);
+    CMemory::InstallCallHook( 0x4A5140, &CDebug::Shutdown, ASM_CALL);
     // Hooked in Nullsub1 leftover
-    CMemory::InstallCallHook(0x401000, (DWORD)&CDebug::DebugAddText, ASM_JMP); 
+    CMemory::InstallCallHook(0x401000, &CDebug::DebugAddText, ASM_JMP); 
     // Hooked in Nullsub16 leftover
-    CMemory::InstallCallHook(0x48A270, (DWORD)&CDebug::DebugAddText, ASM_JMP); 
+    CMemory::InstallCallHook(0x48A270, &CDebug::DebugAddText, ASM_JMP); 
     // Hooked in printf leftover
-    CMemory::InstallCallHook(0x648AC0, (DWORD)&CDebug::DebugAddText, ASM_JMP); 
+    CMemory::InstallCallHook(0x648AC0, &CDebug::DebugAddText, ASM_JMP); 
 
     m_pPathFindHook->ApplyHook();
 }
 
 void CGameHookManager::RemoveHook(void){
     //Restore in CFileLoader::LoadLevel
-    CMemory::InstallCallHook( 0x48DD5D, dwFunc_CFileLoader__LoadScene, ASM_CALL);
+    CMemory::InstallCallHook( 0x48DD5D, (void*)dwFunc_CFileLoader__LoadScene, ASM_CALL);
     //Restore NullSub1
     CMemory::InstallPatch<char>(0x401000, 0xC3);
     //Restore Nullsub16

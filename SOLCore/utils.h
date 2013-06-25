7fc5bb854252603684e24f724d0766aea00c8b6b
Swoorup Joshi <satanic_dementia143@yahoo.com>
2013-06-25 15:40:51 +0800

    Minor Update Removed Duplicate _pad defination MACRO

--- SOLCore/utils.h
+++ SOLCore/utils.h
@@ -4,7 +4,6 @@
 #define M_PI       3.14159265358979323846
 #define ARRLEN(a) (sizeof(a)/sizeof(a[0])) 
 #include <math.h>
-#define _pad(x,n) unsigned char x[n]
 #define OFFSETOF(type, field)    ((unsigned long) &(((type *) 0)->field))
 
 namespace utl{
